7c44878aa7400c454d65f7cda37cfc745daaca25
Alex. Z <azavgo@yahoo.com>
2021-08-15 21:07:30 +1000

    Partial implementation of the operations on types Point and Vector

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,3 +1,6 @@
+use std::ops::{Add, Mul, Sub, Div};
+
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Point {
     x: f32,
     y: f32,
@@ -22,6 +25,51 @@ impl Point {
     }
 }
 
+impl Add for Point {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        Self::new(
+            self.x() + other.x(),
+            self.y() + other.y(),
+            self.z() + other.z(),
+        )
+    }
+}
+
+impl Sub for Point {
+    type Output = Self;
+    fn sub(self, other: Self) -> Self {
+        Self::new(
+            self.x() - other.x(),
+            self.y() - other.y(),
+            self.z() - other.z(),
+        )
+    }
+}
+
+impl Mul<f32> for Point {
+    type Output = Self;
+    fn mul(self, rhs: f32) -> Self {
+        Self::new(self.x() * rhs, self.y() * rhs, self.z() * rhs)
+    }
+}
+
+impl Mul<Point> for f32 {
+    type Output = Point;
+    fn mul(self, rhs: Point) -> Point {
+        Point::new(self * rhs.x(), self * rhs.y(), self * rhs.z())
+    }
+}
+
+//TODO: needs to convert to the Result type to avoid panic at divided by zero error
+impl Div<f32> for Point {
+    type Output = Self;
+    fn div(self, rhs: f32) -> Self {
+        Self::new(self.x() / rhs, self.y() / rhs, self.z() / rhs)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Vector {
     p: Point,
 }
@@ -29,5 +77,112 @@ pub struct Vector {
 impl Vector {
     pub fn new(p: Point) -> Self {
         Self { p: p }
+    }
+
+    pub fn point(self) -> Point {
+        self.p
+    }
+
+    pub fn length(self) -> f32 {
+        (self * self).sqrt()
+    }
+}
+
+impl Add for Vector {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        Self::new(self.point() + other.point())
+    }
+}
+
+impl Sub for Vector {
+    type Output = Self;
+    fn sub(self, other: Self) -> Self {
+        Self::new(self.point() - other.point())
+    }
+}
+
+impl Mul for Vector {
+    type Output = f32;
+    fn mul(self, other: Self) -> f32 {
+        let p1 = self.point();
+        let p2 = other.point();
+        p1.x() * p2.x() + p1.y() * p2.y() + p1.z() * p2.z()
+    }
+}
+
+impl Mul<f32> for Vector {
+    type Output = Self;
+    fn mul(self, rhs: f32) -> Self {
+        Self::new(self.point() * rhs)
+    }
+}
+
+impl Mul<Vector> for f32 {
+    type Output = Vector;
+    fn mul(self, rhs: Vector) -> Vector {
+        Vector::new(self * rhs.point())
+    }
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+    #[test]
+    fn multiply_point_test() {
+        let p = Point::new(2.0, -1.0, 10.0);
+        assert_eq!(
+            Point::new(4.0, -2.0, 20.0),
+            2.0 * Point::new(2.0, -1.0, 10.0)
+        );
+    }
+
+    #[test]
+    fn point_multiply_test() {
+        let p = Point::new(2.0, -1.0, 10.0);
+        assert_eq!(
+            Point::new(4.0, -2.0, 20.0),
+            Point::new(2.0, -1.0, 10.0) * 2.0
+        );
+    }
+
+    #[test]
+    fn multiply_vector_test() {
+        let p = Point::new(2.0, -1.0, 10.0);
+        let v = Vector::new(p);
+        let p_new = Point::new(4.0, -2.0, 20.0);
+        assert_eq!(Vector::new(p_new), 2.0 * v);
+    }
+
+    #[test]
+    fn vector_multiply_test() {
+        let p = Point::new(2.0, -1.0, 10.0);
+        let v = Vector::new(p);
+        let p_new = Point::new(4.0, -2.0, 20.0);
+        assert_eq!(Vector::new(p_new), v * 2.0);
+    }
+
+    #[test]
+    fn vector_scalar_multiply_test() {
+        let p1 = Point::new(2.0, -1.0, 3.0);
+        let v1 = Vector::new(p1);
+        let p2 = Point::new(-2.0, -3.0, -1.0);
+        let v2 = Vector::new(p2);
+        assert_eq!(-4.0, v1 * v2);
+    }
+
+    #[test]
+    fn vector_squared_length_test() {
+        let p = Point::new(2.0, -1.0, 3.0);
+        let v = Vector::new(p);
+        assert_eq!(14.0, v * v);
+    }
+
+    #[test]
+    fn vector_length_test() {
+        let p = Point::new(3.0, 0.0, 0.0);
+        let v = Vector::new(p);
+        assert_eq!(3.0, v.length());
     }
 }
