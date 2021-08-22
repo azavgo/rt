6e9a7a31de95ef07370697573d65111ca3559182
Alex. Z <azavgo@yahoo.com>
2021-08-22 20:04:26 +1000

    Corrected the ppm image file

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Ray Tracer implementation in Rust 
+### Following the "Ray Tracer in One Weekend" by Peter Shirley
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
-use std::io::Error;
+use std::fs::write; 
 
-mod size;
-mod utils; 
-mod colour;
-mod geometry; 
-
-
-use size::Size;
-
-fn main() -> Result<(), Error> {
-    let size = Size::new(256, 256);
-    let canvas = utils::to_canvas(size);
-    utils::to_file("image.ppm".to_string(), canvas, size)?;
-
-    Ok(())
+fn main() {
+    let mut ppm_image = format!("P3\n{} {}\n255\n", 256, 256);
+    
+    let mut r: f64 = 0.0; 
+    let mut g: f64 = 0.0;
+    let mut b: f64 = 0.0;
+    
+    for j in (0..256).rev() {
+        print!("Rendering image: {}%\r", (255 - &j) * 100 / 255);
+        for i in 0..256 {
+            r = i as f64/255.0; 
+            g = j as f64/255.0;
+            b = 0.25; 
+            
+            ppm_image = format!(
+                "{}{} {} {}\n",
+                ppm_image,
+                (255.99 * r) as u8,
+                (255.99 * g) as u8,
+                (255.99 * b) as u8
+            );
+        }
+    }
+    write("output.ppm", ppm_image).unwrap();
 }
